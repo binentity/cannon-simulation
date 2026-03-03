7aaa0e2c81deb0e633318adf5ecdf93fd0227dc5
binentity <binentity@yandex.ru>
2026-03-03 05:10:09 +0300

    Comments clear.

--- main.cpp
+++ main.cpp
@@ -10,29 +10,17 @@ const int SCREEN_FREQUENCY = 144;
 const int WIDTH            = 1920;
 const int HEIGHT           = 1080;
 
+
 const float PI             = 3.14159265f;
 
 const std::string PROJECT_NAME = "Cannon simulation";
 
 int main(int argc, char *argv[]) {
-    
-    // window initialization...
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
     Simulation sim;
-    // notification messages...
-    // cout << "window started..." << endl;
-
     sim.run();
-
-    // static objects...
-    // sf::RectangleShape cannon(sf::Vector2f(50.f, 20.f)); 
-    // cannon.setFillColor(sf::Color::White);
-    // cannon.setPosition(50, 750);
-    // cannon.setOrigin(40, 10);
-
-    // window loop...
 
     return 0;
 }
